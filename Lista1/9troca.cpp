06c882c2dc44176890541c6605d22cce8ee35821
Matheus Figueiredo <alvesmatheusbr100@gmail.com>
2025-04-01 21:32:53 -0300

    Create 9troca.cpp

---
+++ Lista1/9troca.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Digite um número x: ";
+    cin >> x;
+
+    int y;
+    cout << "Digite um número y: ";
+    cin >> y;
+
+    int aux = x;
+
+    cout << "x: " << y << "\n";
+    cout << "y: " << aux << "\n";
+}
